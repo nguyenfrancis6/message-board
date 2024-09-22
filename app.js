6ac477f5392317eaf2acb0e47ddf443541043cc0
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-09-22 16:38:27 -0700

    Routing + form + post method

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
-const express = require('express');
-const path = require('node:path');
-
+const express = require("express");
+const path = require("node:path");
 const app = express();
-app.set('views', path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: new Date(),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
-  }
+    added: new Date(),
+  },
 ];
 
-app.get('/', (req, res) => {
-  res.send("Hello");
-})
+app.get("/", (req, res) => {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
 
-app.get('/new', (req, res) => {
-  res.send("New");
-})
+app.get("/new", (req, res) => {
+  res.render("form");
+});
+
+app.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
 
 const PORT = 3000;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
