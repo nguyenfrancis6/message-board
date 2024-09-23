053b0d842a8d45d9a101c9712ec5b8671f31f993
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-09-22 17:43:37 -0700

    Port changed to environment intially, added go back button

--- app.js
+++ app.js
@@ -36,5 +36,5 @@ app.post("/new", (req, res) => {
   res.redirect("/");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
